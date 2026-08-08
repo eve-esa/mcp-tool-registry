c2285cc0b2d7e9d5d063d22b5b92ce60cfacf51c
JINO-ROHIT <find.jinorohit@gmail.com>
2026-08-08 17:55:53 +0530

    eve retrieval fix: use dev instead of staging

--- servers/eve_retrieval/server.py
+++ servers/eve_retrieval/server.py
@@ -50,7 +50,7 @@ logging.basicConfig(level=logging.INFO, format="%(levelname)s | %(name)s | %(mes
 
 EVE_EMAIL = os.getenv("EVE_EMAIL", "")
 EVE_PASSWORD = os.getenv("EVE_PASSWORD", "")
-EVE_API_BASE_URL = os.getenv("EVE_API_BASE_URL", "https://staging-api.eve-chat.chat")
+EVE_API_BASE_URL = os.getenv("EVE_API_BASE_URL", "https://dev.eve-chat.chat")
 
 _HTTP_TIMEOUT = 120.0
 
